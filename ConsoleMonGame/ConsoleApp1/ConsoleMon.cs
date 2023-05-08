51955e84a99893d0e62971b35fe13dea1c1cc76e
Wes <114735316+Wes34840@users.noreply.github.com>
2023-05-08 12:03:47 +0200

    Classes & Functions opdrachten

---
+++ ConsoleMonGame/ConsoleApp1/ConsoleMon.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class ConsoleMon
+    {
+        internal int health, energy;
+        internal string name;
+
+        internal void TakeDamage(int damage)
+        {
+            health -= damage;
+        }
+
+        internal void DepleteEnergy(int energy)
+        {
+            this.energy -= energy;
+        }
+    }
+}
---
+++ ConsoleMonGame/ConsoleApp1/Skill.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Skill
+    {
+        internal int damage, energyCost;
+        internal string name;
+
+        internal void UseOn(ConsoleMon target, ConsoleMon caster)
+        {
+            caster.DepleteEnergy(energyCost);
+            target.TakeDamage(damage);
+        }
+    }
+}
