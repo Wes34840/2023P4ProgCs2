dac7193d206e8b8c7c133be4bb79e2e6fa26f748
Wes <114735316+Wes34840@users.noreply.github.com>
2023-05-08 14:05:01 +0200

    EnumOpdracht 7

---
+++ EnumOpdracht/EnumOpdracht/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace EnumOpdracht
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Colors myMainThemeColor = Colors.purple;
+            if (myMainThemeColor == Colors.purple)
+            {
+                Console.WriteLine("That is my color!");
+            }
+        }
+    }
+}
