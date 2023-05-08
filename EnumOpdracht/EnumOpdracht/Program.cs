053905dc2ae98bb8d1a8f310700b9bb185fd02e4
Wes <114735316+Wes34840@users.noreply.github.com>
2023-05-08 17:02:05 +0200

    Enum Opdrachten Extra 1 en 2

--- EnumOpdracht/EnumOpdracht/Program.cs
+++ EnumOpdracht/EnumOpdracht/Program.cs
@@ -6,10 +6,29 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-            Colors myMainThemeColor = Colors.purple;
-            if (myMainThemeColor == Colors.purple)
+
+
+            Colors[] ColorList = Enum.GetValues(typeof(Colors)).Cast<Colors>().ToArray();
+
+            foreach (Colors color in ColorList)
             {
-                Console.WriteLine("That is my color!");
+                Console.WriteLine(color.ToString());
+            }
+
+            Console.WriteLine("Choose one and hope for the best");
+            string value = Console.ReadLine();
+
+
+            Colors parsedColor = Enum.Parse<Colors>(value);
+
+
+            if (parsedColor == Colors.purple)
+            {
+                Console.WriteLine("You guessed correctly!");
+            }
+            else
+            {
+                Console.WriteLine("Incorrect, deleting system32...");
             }
         }
     }
