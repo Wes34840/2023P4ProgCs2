033b1dfd6ec0e6966ad23ec5d70dc2f6c0509fdc
Wes <114735316+Wes34840@users.noreply.github.com>
2023-05-09 14:16:04 +0200

    Files and Directories Opdracht

---
+++ FileDirOpdracht/FileDirOpdracht/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace FileDirOpdracht
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            DirectoryInfo dir = new DirectoryInfo(@"C:\Users\sbroe\Documents\Ma\bewijzenmap\module4\m4prog\2023P4ProgCs2");
+            FileInfo[] files = dir.GetFiles();
+
+            foreach (FileInfo file in files)
+            {
+                Console.WriteLine(file.Name);
+            }
+
+            DirectoryInfo[] directories = dir.GetDirectories();
+            foreach(DirectoryInfo directory in directories)
+            {
+                Console.WriteLine(directory.Name);
+                ShowAllInsideDir(directory);
+            }
+        }
+
+        static void ShowAllInsideDir(DirectoryInfo dir)
+        {
+            FileInfo[] files = dir.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                Console.WriteLine(file.Name);
+            }
+            DirectoryInfo[] directories = dir.GetDirectories();
+            if (directories != null)
+            {
+                foreach (DirectoryInfo directory in directories)
+                {
+                    Console.WriteLine(directory.Name);
+                    ShowAllInsideDir(directory);
+                }
+            }
+        } // idk how the fuck I did this first try and didn't brick my pc lmao
+    }
+}
