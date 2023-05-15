8eab2824050267dbc2b3d105486fcc938c957f16
Wes <114735316+Wes34840@users.noreply.github.com>
2023-05-15 11:56:02 +0200

    Opdracht 1, extra & extra2

---
+++ SplitOpdracht/SplitOpdracht/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace SplitOpdracht
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string content = File.ReadAllText(Environment.CurrentDirectory + "\\stringsplit.txt");
+            content = content.Replace(" ", "");
+            string[] keyvalue = content.Split(":");
+            string[] cijferspervak = keyvalue[1].Split(",", StringSplitOptions.RemoveEmptyEntries);
+
+            Console.WriteLine(keyvalue[0]);
+
+            foreach (string cijfer in cijferspervak)
+            {
+                Console.WriteLine(cijfer);
+            }
+
+            
+
+        }
+    }
+}
